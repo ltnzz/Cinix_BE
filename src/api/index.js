499a200fe95ff1ab43a89fd23b4b6bfeaf8bf103
Latanza <latanzaakbarfadilah@gmail.com>
2025-11-23 17:20:46 +0700

    ketinggalan

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 import authRouter from "../routes/authRoute.js";
 import movieRouter from "../routes/movie.route.js";
 import { sessionConfig } from '../utils/sessions.js';
-import { cleanup } from './service/cron.js';
+import { cleanup } from '../service/cron.js';
 
 const app = express();
 app.use(express.json());
@@ -15,6 +15,8 @@ app.use(FormData.parse());
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+
+const allowedOrigin = ["https://CINIX-FE.vercel.app", "http://localhost:5432"];
 
 app.use(
     cors({
