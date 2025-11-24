ee975577b77f7e097684e06559b5afddb6dd2d8c
Latanza <latanzaakbarfadilah@gmail.com>
2025-11-24 22:18:14 +0700

    ketinggalan

--- src/api/index.js
+++ src/api/index.js
@@ -13,7 +13,6 @@ import { cleanup } from '../service/cron.js';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-app.use(FormData.parse());
 app.use(cookieParser());
 cleanup.start();
 
