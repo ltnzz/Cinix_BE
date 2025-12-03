6065d1e8fbd5e6a4a7b5a3d74ad4dd68742a760b
Latanza <latanzaakbarfadilah@gmail.com>
2025-12-03 13:18:17 +0700

    adios amigos semuanya

--- src/controlllers/user.controller.js
+++ src/controlllers/user.controller.js
@@ -5,39 +5,63 @@ export const getRecommendations = async (req, res) => {
         const userId = req.user.id;
 
         const lastPayment = await prisma.payments.findFirst({
-        where: { userId, status: "success" },
-        orderBy: { transaction_time: "desc" },
-        include: { movie: true },
+            where: { 
+                user_id: userId,
+                status: "success" 
+            },
+            orderBy: { transaction_time: "desc" },
+            include: { movie: true },
         });
 
-        console.log("lastPayment:", lastPayment);
+        
         if (!lastPayment) {
-        return res.status(404).json({ message: "Belum ada film yang ditonton" });
+            const randomMovies = await prisma.movies.findMany({ take: 5 });
+            
+            return res.status(200).json({ 
+                success: true,
+                message: "Rekomendasi film populer (New User)",
+                recommendations: randomMovies 
+            });
         }
 
         const watched = await prisma.payments.findMany({
-        where: { user_id: userId, status: "success" },
-        select: { movie_id: true },
+            where: { 
+                user_id: userId, 
+                status: "success" 
+            },
+            select: { movie_id: true },
         });
 
         const watchedMovieIds = watched.map((r) => r.movie_id);
 
-        // Ambil film dengan genre yang sama, tapi belum ditonton
         const recommendedMovies = await prisma.movies.findMany({
-        where: {
-            genre: lastPayment.movie.genre,
-            id_movie: { notIn: watchedMovieIds },
-        },
+            where: {
+                genre: lastPayment.movie.genre,
+                id_movie: { notIn: watchedMovieIds },
+            },
+            take: 20
         });
 
         const randomized = recommendedMovies
-        .sort(() => Math.random() - 0.5)
-        .slice(0, 5);
+            .sort(() => Math.random() - 0.5)
+            .slice(0, 5);
+
+        if (randomized.length === 0) {
+            const fallbackMovies = await prisma.movies.findMany({
+                where: { id_movie: { notIn: watchedMovieIds } },
+                take: 5
+            });
+            return res.json({
+                success: true,
+                based_on: "Might Also Like",
+                recommendations: fallbackMovies
+            });
+        }
 
         return res.json({
-        success: true,
-        based_on: lastPayment.movie.title,
-        recommendations: randomized,
+            success: true,
+            based_on: lastPayment.movie.title,
+            recommendations: randomized,
         });
 
     } catch (err) {
