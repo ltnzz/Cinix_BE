aa791db294f7db6a03718f48286ebeace14dc0cf
Latanza <latanzaakbarfadilah@gmail.com>
2025-12-04 18:09:54 +0700

    fix admin

--- src/controlllers/admin.controller.js
+++ src/controlllers/admin.controller.js
@@ -26,8 +26,8 @@ export const adminLogin = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 6 * 60 * 60 * 1000,
         })
 
